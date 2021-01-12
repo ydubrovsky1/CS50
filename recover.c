4672db7d66b20d0f1c5c707c2bc49571d540f065
ydubrovsky1 <ydubrovsky1@users.noreply.github.com>
2021-01-12 16:30:22 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
- 
- 
+
+
 //define some data types for ease of use
 typedef uint8_t BYTE;
 typedef uint32_t xBYTE;
+const int HUGE = 5120000;
+
 int main(int argc, char *argv[])
 {
     // Check command-line arguments
@@ -14,51 +16,74 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover image\n");
         return 1;
     }
-    
+
     //open memory card file
-    FILE *old_file = fopen(argv[1], "r");
- 
+    FILE* old_file = fopen(argv[1], "r");
+
     //reject if file location invalid
     if (old_file == NULL)
     {
-        printf("invalid file.");
-        return 1; 
+        printf("invalid file.\n");
+        return 1;
     }
+ 
+    //open new file
+    FILE* new_file = fopen("newfile", "w");
     
-    //read the old file into a buffer array until reach end of the file
-    BYTE buffer[512 * 60];
-    while (fread(buffer, sizeof(BYTE), 512 * 60, old_file) != 0)
-    {
-        fread(buffer, sizeof(BYTE), 512 * 60, old_file);
-    }
-    //read through the array looking for JPEGs
-    for (int i = 0; i <(512 * 60); i = (i + 512))
+    //read the old file into the buffer location until reach end of the file
+    BYTE buffer[512];
+    int counter = 0;
+    while (fread(buffer, sizeof(BYTE), 512, old_file) != 0)
     {
         //if this is the beginning of a JPEG...
-        if (buffer[i] == 0xe0 && buffer [i + 1] == 0xd8 && buffer [i + 2] == 0xff && buffer [i + 3] == (0xe0 | 0xe1 | 0xe2 | 0xe3 | 0xe4 | 0xe5 | 0xe6 | 0xe7 | 0xe8 | 0xe9 | 0xea | 0xeb | 0xec | 0xed | 0xee | 0xef))
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //open new file to write to 
-            char name[8];
-            sprintf(name, "%i.jpg", i);
-            FILE *new_file = fopen(name , "w");
-            //write the 512 byte chunk into the new file
-            fwrite(buffer, sizeof(BYTE), 512, new_file);
-            //loop through subsequent chunks, continuing to write. if get to new JPEG, exit this loop
-            for (int j = (i + 512); j <(512*60); j = j + 152)
+            if (counter == 0)
             {
-                if (buffer[j] == 0xe0 && buffer [j + 1] == 0xd8 && buffer [j + 2] == 0xff && buffer [j + 3] == (0xe0 | 0xe1 | 0xe2 | 0xe3 | 0xe4 | 0xe5 | 0xe6 | 0xe7 | 0xe8 | 0xe9 | 0xea | 0xeb | 0xec | 0xed | 0xee | 0xef))
-                {
-                    j = (512 * 60);
-                }
-                else
-                {
-                    fwrite(buffer, sizeof(BYTE), 512, new_file);
-                }
+                //close old new_file
+                fclose(new_file);
                 
+                //open new file to write to
+                char name[8];
+                sprintf(name, "%03i.jpg", counter);
+                new_file = fopen(name, "w");
+           
+                //write the 512 byte chunk into the new file
+                fwrite(buffer, sizeof(BYTE), 512, new_file);
+                
+                //add to the file counter
+                counter++;
             }
+            //if this is not the first beginning of JPEG
+            else
+            {
+                //close the previous file
+                fclose(new_file);
+            
+                //open new file to write to
+                char name[8];
+                sprintf(name, "%03i.jpg", counter);
+                new_file = fopen(name, "w");
+            
+                //write the 512 byte chunk into the new file
+                fwrite(buffer, sizeof(BYTE), 512, new_file);
+                
+                //add to the file counter
+                counter++;
+            }
+            
+        }
+        //if not beginning of JPEG
+        else 
+        {
+            //write information to current file
+            fwrite(buffer, sizeof(BYTE), 512, new_file);
         }
     }
-    
+
     //close old file
     fclose(old_file);
+    
+    //close most recent new file
+    fclose(new_file);
 }
