11fbbca31cbd3fbc2a6d9fab25d5a51690a3d74a
ydubrovsky1 <ydubrovsky1@users.noreply.github.com>
2021-01-19 20:15:47 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -126,7 +126,11 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-   return counter; 
+   if(counter > 0)
+        return counter; 
+    else
+        return 0;
+    
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
