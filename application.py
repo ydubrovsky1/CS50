d485094c824a8490d789e2ad7fd72cce7b5e5348
ydubrovsky1 <ydubrovsky1@users.noreply.github.com>
2021-02-15 16:09:41 +0000

    automated commit by check50 [check50=True]

--- application.py
+++ application.py
@@ -211,15 +211,15 @@ def register():
         
         #Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
             
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
             
         #Ensure password and confirmation match
         elif request.form.get("password") != request.form.get("confirmation"):
-             return apology("password and confirmation do not match", 403)
+             return apology("password and confirmation do not match", 400)
             
         #Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
